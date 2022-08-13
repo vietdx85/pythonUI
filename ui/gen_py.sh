69ac06f4064c73836f005e5fc0a6a50db56d23f4
vietdx85 <vietdx85@gmail.com>
2022-08-13 09:12:29 +0700

    add gen_py to gen py from ui

---
+++ ui/gen_py.sh
@@ -0,0 +1,5 @@
+pyuic5.exe home.ui -o ../home.py
+pyuic5.exe red.ui -o ../red.py
+pyuic5.exe yellow.ui -o ../yellow.py
+pyuic5.exe green.ui -o ../green.py
+pyrcc5.exe picture.qrc -o ../picture_rc.py
